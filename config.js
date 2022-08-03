7a67c82a2c215a209e16b1fc955125d60edc5740
Navau <milibrolunadepluton344@gmail.com>
2022-08-03 16:37:29 -0400

    Avances 3/8/22

--- config.js
+++ config.js
@@ -2,15 +2,15 @@ const API_VERSION = "v1";
 const IP_SERVER_API = "localhost";
 // const IP_SERVER_DB = "localhost";
 // const PORT_DB = process.env.DATABASE_PORT || 5432;
-const IP_SERVER_DB = "ec2-54-157-16-196.compute-1.amazonaws.com";
+const IP_SERVER_DB = "ec2-54-208-104-27.compute-1.amazonaws.com";
 const PORT_DB = 5432;
 const PORT_SERVER = process.env.PORT || 3977; // 3977 || 5290
 
 const PARAMS_CONNECTION = {
   host: IP_SERVER_DB,
-  user: "svtwlzpatpgvdg",
-  password: "71b620b8896975475949c07a703f156fe6cc96a6b2b2d1042319fbd9c410ae1c", //apsadmin2022
-  database: "dfpc86sb3fh4vd",
+  user: "ykhduorprqqmpc",
+  password: "5b12f443b747ce6f316951a669a769f2a63aa0cf7af8767342718f5754d5803e", //apsadmin2022
+  database: "d6oj32u04ihb07",
   port: PORT_DB,
   ssl: { rejectUnauthorized: false },
 };
