f1271e64f409204fb2fa53f8b78749f4bbc1b92b
Navau <milibrolunadepluton344@gmail.com>
2022-08-02 23:25:46 -0400

    Avances 2/8/22

--- config.js
+++ config.js
@@ -6,11 +6,9 @@ const PORT_DB = process.env.DATABASE_PORT || 5432;
 const PORT_SERVER = 3977; // 3977 || 5290
 const PARAMS_CONNECTION = {
   host: IP_SERVER_DB,
-  user: process.env.DATABASE_USER || "svtwlzpatpgvdg",
-  password:
-    process.env.DATABASE_PASS ||
-    "71b620b8896975475949c07a703f156fe6cc96a6b2b2d1042319fbd9c410ae1c", //apsadmin2022
-  database: process.env.DATABASE_NAME || "dfpc86sb3fh4vd",
+  user: "svtwlzpatpgvdg",
+  password: "71b620b8896975475949c07a703f156fe6cc96a6b2b2d1042319fbd9c410ae1c", //apsadmin2022
+  database: "dfpc86sb3fh4vd",
   port: PORT_DB,
   ssl: { rejectUnauthorized: false },
 };
