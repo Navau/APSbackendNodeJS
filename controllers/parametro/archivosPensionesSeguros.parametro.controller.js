7dea456e3dbcb54a0e355732a1991711787d449f
Navau <milibrolunadepluton344@gmail.com>
2022-08-18 13:50:07 -0400

    Avances 18/8/22 PRUEBAS

--- controllers/parametro/archivosPensionesSeguros.parametro.controller.js
+++ controllers/parametro/archivosPensionesSeguros.parametro.controller.js
@@ -129,75 +129,108 @@ async function SeleccionarArchivosBolsa(req, res) {
     } else if (_tipoDeCambio.ok === false) {
       respResultadoVacio404END(res, _tipoDeCambio.message);
       return;
-    } else {
-      let queryFeriado = `SELECT CASE WHEN  EXTRACT(DOW FROM TIMESTAMP'${fecha_operacion}' ) IN (6,7) OR
-      (SELECT COUNT(*) FROM public."APS_param_feriado" WHERE fecha = '${fecha_operacion}') > 0
-       THEN 0 ELSE 1 END;`;
-      const workingDay = await pool
-        .query(queryFeriado)
-        .then((result) => {
-          return result.rows;
-        })
-        .catch((err) => {
-          console.log(err);
-          respErrorServidor500END(res, err);
-          return null;
-        });
-
-      let periodicidad = [154]; //VALOR POR DEFECTO
-
-      console.log(workingDay);
-
-      if (parseInt(workingDay?.[0].case) === 0) {
-        periodicidad = [154]; // DIARIOS
-      } else {
-        periodicidad = [154, 219]; // DIAS HABILES
-      }
-
-      let query = `SELECT replace(replace(replace(replace(replace(replace(replace(replace(replace(
-    "APS_param_archivos_pensiones_seguros".nombre::text, 
-    'nnn'::text, "APS_seg_institucion".codigo::text),
-    'aaaa'::text, EXTRACT(year FROM TIMESTAMP '${fecha_operacion}')::text),
-    'mm'::text, lpad(EXTRACT(month FROM TIMESTAMP '${fecha_operacion}')::text, 2, '0'::text)),
-    'dd'::text, lpad(EXTRACT(day FROM TIMESTAMP '${fecha_operacion}')::text, 2, '0'::text)),
-    'AA'::text, substring(EXTRACT(year FROM TIMESTAMP '${fecha_operacion}')::text from 3 for 2)),
-    'MM'::text, lpad(EXTRACT(month FROM TIMESTAMP '${fecha_operacion}')::text, 2, '0'::text)),
-    'DD'::text, lpad(EXTRACT(day FROM TIMESTAMP '${fecha_operacion}')::text, 2, '0'::text)),
-    'nntt'::text, "APS_seg_institucion".codigo::text ||
-    "APS_param_archivos_pensiones_seguros".codigo::text),
-    'nn'::text, "APS_seg_institucion".codigo::text) AS archivo,
-    "APS_seg_usuario".id_usuario,
-    "APS_param_archivos_pensiones_seguros".archivo_vacio 
-    FROM "APS_param_archivos_pensiones_seguros" 
-    JOIN "APS_param_clasificador_comun" 
-    ON "APS_param_archivos_pensiones_seguros".id_periodicidad = "APS_param_clasificador_comun".id_clasificador_comun 
-    JOIN "APS_seg_usuario_rol" 
-    ON "APS_seg_usuario_rol".id_rol = "APS_param_archivos_pensiones_seguros".id_rol 
-    JOIN "APS_seg_usuario" 
-    ON "APS_seg_usuario".id_usuario = "APS_seg_usuario_rol".id_usuario 
-    JOIN "APS_seg_institucion" 
-    ON "APS_seg_institucion".id_institucion = "APS_seg_usuario".id_institucion 
-    WHERE "APS_param_clasificador_comun".id_clasificador_comun in (${periodicidad.join()}) 
-    AND "APS_seg_usuario".id_usuario = '${id_usuario}' 
-    AND "APS_seg_usuario_rol".id_rol = '${id_rol}' 
-    AND "APS_param_archivos_pensiones_seguros".status = true;`;
-
-      console.log(query);
-
-      pool
-        .query(query)
-        .then((result) => {
-          if (!result.rowCount || result.rowCount < 1) {
-            respResultadoVacio404(res);
-          } else {
-            respResultadoCorrectoObjeto200(res, result.rows);
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-          respErrorServidor500END(res, err);
-        });
     }
+    let queryFeriado = EscogerInternoUtil("APS_param_feriado", {
+      select: ["*"],
+      where: [
+        {
+          key: "fecha",
+          value: fecha_operacion,
+        },
+      ],
+    });
+    const holidays = await pool
+      .query(queryFeriado)
+      .then((result) => {
+        return result.rows;
+      })
+      .catch((err) => {
+        console.log(err);
+        respErrorServidor500END(res, err);
+        return null;
+      });
+
+    if (holidays === null) {
+      return null;
+    }
+
+    const fechaOperacion = new Date(fecha_operacion);
+    const day = fechaOperacion.getUTCDay();
+    let periodicidad = [154]; //VALOR POR DEFECTO
+
+    if (day === 0 || day === 6 || holidays.length >= 1) {
+      periodicidad = [154]; // DIARIOS
+    } else {
+      periodicidad = [154, 219]; // DIAS HABILES
+    }
+    // let queryFeriado = `SELECT CASE WHEN  EXTRACT(DOW FROM TIMESTAMP'${fecha_operacion}' ) IN (6,7) OR
+    // (SELECT COUNT(*) FROM public."APS_param_feriado" WHERE fecha = '${fecha_operacion}') > 0
+    //  THEN 0 ELSE 1 END;`;
+    // console.log(queryFeriado);
+    // const workingDay = await pool
+    //   .query(queryFeriado)
+    //   .then((result) => {
+    //     return result.rows;
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //     respErrorServidor500END(res, err);
+    //     return null;
+    //   });
+
+    // let periodicidad = [154]; //VALOR POR DEFECTO
+
+    // console.log(workingDay);
+
+    // if (parseInt(workingDay?.[0].case) === 0) {
+    //   periodicidad = [154]; // DIARIOS
+    // } else {
+    //   periodicidad = [154, 219]; // DIAS HABILES
+    // }
+
+    let query = `SELECT replace(replace(replace(replace(replace(replace(replace(replace(replace(
+  "APS_param_archivos_pensiones_seguros".nombre::text, 
+  'nnn'::text, "APS_seg_institucion".codigo::text),
+  'aaaa'::text, EXTRACT(year FROM TIMESTAMP '${fecha_operacion}')::text),
+  'mm'::text, lpad(EXTRACT(month FROM TIMESTAMP '${fecha_operacion}')::text, 2, '0'::text)),
+  'dd'::text, lpad(EXTRACT(day FROM TIMESTAMP '${fecha_operacion}')::text, 2, '0'::text)),
+  'AA'::text, substring(EXTRACT(year FROM TIMESTAMP '${fecha_operacion}')::text from 3 for 2)),
+  'MM'::text, lpad(EXTRACT(month FROM TIMESTAMP '${fecha_operacion}')::text, 2, '0'::text)),
+  'DD'::text, lpad(EXTRACT(day FROM TIMESTAMP '${fecha_operacion}')::text, 2, '0'::text)),
+  'nntt'::text, "APS_seg_institucion".codigo::text ||
+  "APS_param_archivos_pensiones_seguros".codigo::text),
+  'nn'::text, "APS_seg_institucion".codigo::text) AS archivo,
+  "APS_seg_usuario".id_usuario,
+  "APS_param_archivos_pensiones_seguros".archivo_vacio 
+  FROM "APS_param_archivos_pensiones_seguros" 
+  JOIN "APS_param_clasificador_comun" 
+  ON "APS_param_archivos_pensiones_seguros".id_periodicidad = "APS_param_clasificador_comun".id_clasificador_comun 
+  JOIN "APS_seg_usuario_rol" 
+  ON "APS_seg_usuario_rol".id_rol = "APS_param_archivos_pensiones_seguros".id_rol 
+  JOIN "APS_seg_usuario" 
+  ON "APS_seg_usuario".id_usuario = "APS_seg_usuario_rol".id_usuario 
+  JOIN "APS_seg_institucion" 
+  ON "APS_seg_institucion".id_institucion = "APS_seg_usuario".id_institucion 
+  WHERE "APS_param_clasificador_comun".id_clasificador_comun in (${periodicidad.join()}) 
+  AND "APS_seg_usuario".id_usuario = '${id_usuario}' 
+  AND "APS_seg_usuario_rol".id_rol = '${id_rol}' 
+  AND "APS_param_archivos_pensiones_seguros".status = true;`;
+
+    console.log(query);
+
+    pool
+      .query(query)
+      .then((result) => {
+        if (!result.rowCount || result.rowCount < 1) {
+          respResultadoVacio404(res);
+        } else {
+          respResultadoCorrectoObjeto200(res, result.rows);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        respErrorServidor500END(res, err);
+      });
   }
 }
 
