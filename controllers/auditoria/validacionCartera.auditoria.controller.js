5eef1b3f65ba8178dd5b7dea6dab27f2d69adf15
Navau <milibrolunadepluton@gmail.com>
2022-12-09 10:49:27 -0400

    09/12/22

--- controllers/auditoria/validacionCartera.auditoria.controller.js
+++ controllers/auditoria/validacionCartera.auditoria.controller.js
@@ -3,16 +3,29 @@ const { EjecutarProcedimientoSQL } = require("../../utils/consulta.utils");
 const {
   respErrorServidor500END,
   respResultadoCorrectoObjeto200,
+  respDatosNoRecibidos400,
 } = require("../../utils/respuesta.utils");
 
 async function Validar(req, res) {
   const { fecha } = req.body;
+  const { id_rol } = req.user;
   const params = {
     body: {
       fecha,
     },
   };
-  const query = EjecutarProcedimientoSQL(`aps_proc_valoracion_cartera`, params);
+  const query =
+    id_rol === 7
+      ? EjecutarProcedimientoSQL(
+          `aps_proc_valoracion_cartera_pensiones`,
+          params
+        )
+      : id_rol === 10
+      ? EjecutarProcedimientoSQL(`aps_proc_valoracion_cartera_seguros`, params)
+      : null;
+  if (query === null) {
+    respDatosNoRecibidos400(res, "El rol debe ser 10 o 7");
+  }
 
   await pool
     .query(query)
