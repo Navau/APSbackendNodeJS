399ba082381c2b595af4216b0303a8e5fdecfb09
Navau <milibrolunadepluton344@gmail.com>
2023-09-22 16:27:41 -0400

    22/09/23

--- controllers/upload/upload.upload.controller.js
+++ controllers/upload/upload.upload.controller.js
@@ -299,7 +299,7 @@ async function CargarArchivo2(req, res) {
         id_carga_archivos: nuevaCarga.id_carga_archivos,
         mensaje: `Envió satisfactorio`,
         fecha_operacion,
-        fecha_carga: DateTime.fromJSDate(nuevaCarga.fecha_carga).toFormat(
+        fecha_carga: DateTime.fromJSDate(nuevaCarga.fecha_carga, { zone: 'America/La_Paz' }).toFormat(
           "yyyy-MM-dd | HH:mm:ss"
         )
       });
